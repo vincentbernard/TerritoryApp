a7446b5e19ff2e7ae85c3772b45c5ff00415086c
Vincent Bernard <vince@bernardtechpartners.com>
2017-01-13 23:14:30 -0500

    added php coding

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "vbernard";
+$password = "Jhmy8frZ#";
+
+// Create connection
+$db = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+} 
+?>
 <html>
 <head lang = "en-US">
 <meta charset = "UTF-8">
-<title>Territory App</title>
+<title>TerritoryTracker</title>
  <link rel = "stylesheet"
    type = "text/css"
    href = "territory.css" />
@@ -21,8 +34,23 @@
 <div class="dropdown">
   <button onclick="myCriteriaFunction()" class="dropbtn" id="criteriabtn">Criteria</button>
   <div id="myCriteriaDropdown" class="dropdown-content">
-  //to be filled by the click of the search button
+  <?php
+//Step2
+$query = "SELECT firstname FROM territorytracker.publisher";
+//mysqli_query($db, $query) or die('Error querying database.');
+
+
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_array($result)) {
+ echo '<a href="#" onclick="">' .$row['firstname'] . '</a>' ;
+}    
+    
+?> 
   </div>
 </div>
+
+   
 </body>
 </html>
